4a074441103d5b1f246e5c4102ea6c94ec48e730
Sarah Francis <sarah.katherinefrancis@gmail.com>
2023-09-14 17:30:35 -0400

    Refactor Checkout to use useRef for inputs

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,41 +1,57 @@
-import React from "react";
+import { useRef } from "react";
 
 import classes from "./Checkout.module.css";
 
 // Define a functional component named Checkout, which takes props as its argument.
 const Checkout = (props) => {
+  // Create refs for input fields to access their values.
+  const nameInputRef = useRef();
+  const streetInputRef = useRef();
+  const postalCodeInputRef = useRef();
+  const cityInputRef = useRef();
+
   // Define a function called confirmHandler that takes an event object as its argument.
   const confirmHandler = (event) => {
     // Prevent the default behavior of the event, which is form submission in this case.
     event.preventDefault();
+
+    // Get the values entered by the user in the input fields.
+    const enteredName = nameInputRef.current.value;
+    const enteredStreet = streetInputRef.current.value;
+    const enteredPostalCode = postalCodeInputRef.current.value;
+    const enteredCity = cityInputRef.current.value;
+
+    // At this point, you can use the entered values as needed, e.g., send them to a server.
   };
 
   // Return the JSX (JavaScript XML) code for rendering the Checkout component.
   return (
-    <form onSubmit={confirmHandler}>
+    <form className={classes.form} onSubmit={confirmHandler}>
       {/* Each block represents a form input field with a label */}
       <div className={classes.control}>
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" />
+        <input type="text" id="name" ref={nameInputRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor="street">Street</label>
-        <input type="text" id="street" />
+        <input type="text" id="street" ref={streetInputRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor="postal">Postal Code</label>
-        <input type="text" id="postal" />
+        <input type="text" id="postal" ref={postalCodeInputRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor="city">City</label>
-        <input type="text" id="city" />
+        <input type="text" id="city" ref={cityInputRef} />
       </div>
-      {/* Button for canceling the form, onClick event triggers the onCancel function from props */}
-      <button type="button" onClick={props.onCancel}>
-        Cancel
-      </button>
-      {/* Button for confirming and submitting the form */}
-      <button>Confirm</button>
+      <div className={classes.actions}>
+        {/* Button for canceling the form, onClick event triggers the onCancel function from props */}
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        {/* Button for confirming and submitting the form */}
+        <button className={classes.submit}>Confirm</button>
+      </div>
     </form>
   );
 };
